ab11eb81e529953df1def311a3e43a82a8d4f02b
liudong8 <dongna@staff.weibo.com>
2018-10-23 20:47:10 +0800

    1. use ref as arg to verify std::forward 2. use auto to receive function return value

--- threadpool/main.cc
+++ threadpool/main.cc
@@ -9,29 +9,35 @@ int add (int a, int b) {
     return a + b;
 }
 
-int minos (int a, int b) {
-    return a - b;
+std::string minos (int a, int b) {
+    return std::to_string(a - b);
 }
 
 int multiple (int a, int b) {
     return a * b;
 }
 
-int division (int a, int b) {
-    return a / b;
+std::string division (int &a, int &b) {
+    a = 10;
+    b = 100;
+    return std::to_string(a / b);
 }
 
 int main () {
     std::unique_ptr<ThreadPool> tp(new ThreadPool(get_nprocs() * 2));
 
     std::future<int> addres = tp->commit(add, 1, 2);
-    std::future<int> minosres = tp->commit(minos, 1, 2);
-    std::future<int> multipleres = tp->commit(multiple, 1, 2);
-    std::future<int> divisionres = tp->commit(division, 1, 2);
+    std::future<std::string> minosres = tp->commit(minos, 1, 2);
+    auto multipleres = tp->commit(multiple, 1, 2);
+    int a = 1, b = 2;
+    std::future<std::string> divisionres = tp->commit(division, std::ref(a), std::ref(b));
 
     std::cout << addres.get() << std::endl;
     std::cout << minosres.get() << std::endl;
     std::cout << multipleres.get() << std::endl;
     std::cout << divisionres.get() << std::endl;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
     return 0;
 }
